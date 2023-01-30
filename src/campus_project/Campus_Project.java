bc3cdce6687b6c7d5e0950487368acf0693e2005
Aman Jain <83697226+amanjain5132@users.noreply.github.com>
2023-01-30 21:35:16 +0530

    Add files via upload

---
+++ src/campus_project/Campus_Project.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package campus_project;
+
+/**
+ *
+ * @author amanj
+ */
+public class Campus_Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        Login l = new Login();
+        l.setVisible(true);
+    }
+    
+}
